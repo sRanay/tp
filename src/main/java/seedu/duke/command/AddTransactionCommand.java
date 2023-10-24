22d7aaf28d2db8c7f08f927339dee301c2c523b4
Jonoans <jonjontan610@hotmail.com>
2023-10-24 20:31:37 +0800

    Parse dates in input

--- src/main/java/seedu/duke/command/AddTransactionCommand.java
+++ src/main/java/seedu/duke/command/AddTransactionCommand.java
@@ -6,6 +6,7 @@ import seedu.duke.exception.DukeException;
 import seedu.duke.parser.Parser;
 import seedu.duke.ui.Ui;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 
 public abstract class AddTransactionCommand extends Command {
@@ -13,10 +14,12 @@ public abstract class AddTransactionCommand extends Command {
         Ui.LIST_COLUMN_WIDTH, Ui.COLUMN_WIDTH, Ui.COLUMN_WIDTH, Ui.COLUMN_WIDTH
     };
     protected static final String AMOUNT_ARG = "amount";
+    protected static final String DATE_ARG = "date";
     protected static final String RECURRENCE_ARG = "recurrence";
     private static final String MISSING_DESC = "Description cannot be empty...";
     private static final String MISSING_AMOUNT = "Amount cannot be empty...";
     private static final String BAD_AMOUNT = "Invalid amount value specified...";
+    private static final String BAD_DATE = "Invalid date specified...";
     private static final String BAD_RECURRENCE = "Invalid recurrence period specified...";
     private boolean isValidated = false;
 
@@ -29,7 +32,8 @@ public abstract class AddTransactionCommand extends Command {
 
         String description = getDescription();
         Double amount = Parser.parseNonNegativeDouble(getArg(AMOUNT_ARG));
-        Transaction transaction = new Transaction(description, amount, null);
+        LocalDate date = Parser.parseDate(getArg(DATE_ARG));
+        Transaction transaction = new Transaction(description, amount, date);
 
         String recurrenceValue = getArg(RECURRENCE_ARG);
         if (recurrenceValue != null) {
@@ -52,6 +56,11 @@ public abstract class AddTransactionCommand extends Command {
         String amountArg = getArg(AMOUNT_ARG);
         if (amountArg == null || amountArg.isBlank()) {
             throw new DukeException(MISSING_AMOUNT);
+        }
+
+        String date = getArg(DATE_ARG);
+        if (date != null && Parser.parseDate(date) == null) {
+            throw new DukeException(BAD_DATE);
         }
 
         Double amount = Parser.parseNonNegativeDouble(amountArg);
