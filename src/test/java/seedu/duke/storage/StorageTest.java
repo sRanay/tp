d72f11a031e24034bb8ea234cc1dbf9b63cdb11b
sRanay <jsong2506@gmail.com>
2023-10-29 18:23:02 +0800

    Add test for loading with valid storage files

--- src/test/java/seedu/duke/storage/StorageTest.java
+++ src/test/java/seedu/duke/storage/StorageTest.java
@@ -1,12 +1,20 @@
 package seedu.duke.storage;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import seedu.duke.classes.StateManager;
+import seedu.duke.command.HelpCommand;
+import seedu.duke.command.ListCommand;
 import seedu.duke.exception.DukeException;
+import seedu.duke.parser.Parser;
+import seedu.duke.ui.Ui;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -21,10 +29,6 @@ public class StorageTest {
         storage = new Storage();
     }
 
-    @AfterEach()
-    void clearState() {
-        storage = null;
-    }
     @Test
     void validRowWithEmptyValues() {
         String[] row = {"TEST1", ""};
@@ -73,5 +77,100 @@ public class StorageTest {
         String testFileName = "filename";
         LocalDate date = LocalDate.parse("25/10/2023", FORMATTER);
         assertEquals(date ,storage.validDate(dateStr, testFileName));
+    }
+
+    @Test
+    void validBooleanWithCorrectBoolString() {
+        String input = "True";
+        assertEquals(true, storage.validBoolean(input));
+        input = "TRUE";
+        assertEquals(true, storage.validBoolean(input));
+        input = "true";
+        assertEquals(true, storage.validBoolean(input));
+        input = "False";
+        assertEquals(true, storage.validBoolean(input));
+        input = "FALSE";
+        assertEquals(true, storage.validBoolean(input));
+        input = "false";
+        assertEquals(true, storage.validBoolean(input));
+    }
+
+    @Test
+    void validBooleanWithWrongBoolString() {
+        String input = "test";
+        assertEquals(false, storage.validBoolean(input));
+    }
+
+    @Test
+    void loadWithNoStorageFile() {
+        assertThrows(DukeException.class, () -> {
+            storage.load();
+        });
+    }
+
+    @Nested
+    class WithValidStorage {
+        @BeforeEach
+        void copyFiles() throws IOException {
+            File src = new File("./TestCSV/valid/category-store.csv");
+            File dst = new File("category-store.csv");
+            Files.copy(src.toPath(), dst.toPath());
+            src = new File("./TestCSV/valid/goal-store.csv");
+            dst = new File("goal-store.csv");
+            Files.copy(src.toPath(), dst.toPath());
+            src = new File("./TestCSV/valid/expense-store.csv");
+            dst = new File("expense-store.csv");
+            Files.copy(src.toPath(), dst.toPath());
+            src = new File("./TestCSV/valid/income-store.csv");
+            dst = new File("income-store.csv");
+            Files.copy(src.toPath(), dst.toPath());
+        }
+
+        @AfterEach
+        void clearStateManager() {
+            File file = new File("category-store.csv");
+            file.delete();
+            file = new File("goal-store.csv");
+            file.delete();
+            file = new File("expense-store.csv");
+            file.delete();
+            file = new File("income-store.csv");
+            file.delete();
+            StateManager.clearStateManager();
+        }
+        @Test
+        void loadWithValidStorageFile() throws DukeException {
+            storage.load();
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            Parser parser = new Parser();
+            Ui ui = new Ui(outputStream);
+            String userInput = "list /type in";
+            HashMap<String, String> args = parser.getArguments(userInput);
+            String commandWord = parser.getDescription(userInput);
+            ListCommand command = new ListCommand(commandWord, args);
+            command.execute(ui);
+            userInput = "list /type out";
+            args = parser.getArguments(userInput);
+            commandWord = parser.getDescription(userInput);
+            command = new ListCommand(commandWord, args);
+            command.execute(ui);
+            assertEquals("Alright! Displaying 3 transactions.\n"
+                            + "=============================== IN TRANSACTIONS ================================\n"
+                            + "ID    Description                      Date         Amount       Goal\n"
+                            + "1     part-time job                    2023-10-29   1000.00      car\n"
+                            + "2     allowance                        2023-10-29   500.00       car\n"
+                            + "3     sell stuff                       2023-10-29   50.00        ps5\n"
+                            + "=============================== IN TRANSACTIONS ================================\n"
+                            + "Alright! Displaying 3 transactions.\n"
+                            + "=============================== OUT TRANSACTIONS ===============================\n"
+                            + "ID    Description                      Date         Amount       Category\n"
+                            + "1     buy dinner                       2023-10-29   15.00        food\n"
+                            + "2     popmart                          2023-10-29   12.00        toy\n"
+                            + "3     grab                             2023-10-29   20.00        transport\n"
+                            + "=============================== OUT TRANSACTIONS ===============================\n"
+                    , outputStream.toString());
+
+
+        }
     }
 }
