db55d5c1e0fb475095caed89c1407b9e6a8fc06c
Jason <jsong2506@gmail.com>
2023-10-29 23:25:10 +0800

    Add debug message

--- src/test/java/seedu/duke/storage/StorageTest.java
+++ src/test/java/seedu/duke/storage/StorageTest.java
@@ -342,6 +342,9 @@ public class StorageTest {
         void saveDataWorking() throws DukeException, IOException {
             storage.save();
             File output = new File("category-store.csv");
+            if(output.exists()) {
+                System.out.println("output exist");
+            }
             File testFile = new File("./TestCSV/valid/category-store.csv");
             assertEquals(true, FileUtils.contentEquals(output, testFile));
             output = new File("goal-store.csv");
