6e0361f5393bc6f374c41977cde5357a77beb04d
sRanay <jsong2506@gmail.com>
2023-10-29 20:02:06 +0800

    Add test for saving data to storage files

--- src/test/java/seedu/duke/storage/StorageTest.java
+++ src/test/java/seedu/duke/storage/StorageTest.java
@@ -1,8 +1,10 @@
 package seedu.duke.storage;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import seedu.duke.classes.StateManager;
-import seedu.duke.command.HelpCommand;
 import seedu.duke.command.ListCommand;
 import seedu.duke.exception.DukeException;
 import seedu.duke.parser.Parser;
@@ -15,7 +17,9 @@ import java.nio.file.Files;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.Scanner;
 
+import org.apache.commons.io.FileUtils;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -140,8 +144,8 @@ public class StorageTest {
         }
         @Test
         void loadWithValidStorageFile() throws DukeException {
-            storage.load();
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            storage.load();
             Parser parser = new Parser();
             Ui ui = new Ui(outputStream);
             String userInput = "list /type in";
@@ -206,8 +210,8 @@ public class StorageTest {
         }
         @Test
         void loadWithEmptyColumns() throws DukeException {
-            storage.load();
             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            storage.load();
             Parser parser = new Parser();
             Ui ui = new Ui(outputStream);
             String userInput = "list /type in";
@@ -235,8 +239,121 @@ public class StorageTest {
                             + "3     grab                             2023-10-29   20.00        transport\n"
                             + "=============================== OUT TRANSACTIONS ===============================\n"
                     , outputStream.toString());
+        }
+    }
+    @Nested
+    class WithErrorColumns {
+        @BeforeEach
+        void copyFiles() throws IOException {
+            File src = new File("./TestCSV/error/category-store.csv");
+            File dst = new File("category-store.csv");
+            Files.copy(src.toPath(), dst.toPath());
+            src = new File("./TestCSV/error/goal-store.csv");
+            dst = new File("goal-store.csv");
+            Files.copy(src.toPath(), dst.toPath());
+            src = new File("./TestCSV/error/expense-store.csv");
+            dst = new File("expense-store.csv");
+            Files.copy(src.toPath(), dst.toPath());
+            src = new File("./TestCSV/error/income-store.csv");
+            dst = new File("income-store.csv");
+            Files.copy(src.toPath(), dst.toPath());
+        }
 
+        @AfterEach
+        void clearStateManager() {
+            File file = new File("category-store.csv");
+            file.delete();
+            file = new File("goal-store.csv");
+            file.delete();
+            file = new File("expense-store.csv");
+            file.delete();
+            file = new File("income-store.csv");
+            file.delete();
+            StateManager.clearStateManager();
+        }
 
+        @Test
+        void loadWithEmptyColumns() throws DukeException {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            storage.load();
+            Parser parser = new Parser();
+            Ui ui = new Ui(outputStream);
+            String userInput = "list /type in";
+            HashMap<String, String> args = parser.getArguments(userInput);
+            String commandWord = parser.getDescription(userInput);
+            ListCommand command = new ListCommand(commandWord, args);
+            command.execute(ui);
+            userInput = "list /type out";
+            args = parser.getArguments(userInput);
+            commandWord = parser.getDescription(userInput);
+            command = new ListCommand(commandWord, args);
+            command.execute(ui);
+            assertEquals("Alright! Displaying 3 transactions.\n"
+                            + "=============================== IN TRANSACTIONS ================================\n"
+                            + "ID    Description                      Date         Amount       Goal\n"
+                            + "1     part-time job                    2023-10-29   1000.00      car\n"
+                            + "2     allowance                        2023-10-29   500.00       car\n"
+                            + "3     sell stuff                       2023-10-29   50.00        ps5\n"
+                            + "=============================== IN TRANSACTIONS ================================\n"
+                            + "Alright! Displaying 3 transactions.\n"
+                            + "=============================== OUT TRANSACTIONS ===============================\n"
+                            + "ID    Description                      Date         Amount       Category\n"
+                            + "1     buy dinner                       2023-10-29   15.00        food\n"
+                            + "2     popmart                          2023-10-29   12.00        toy\n"
+                            + "3     grab                             2023-10-29   20.00        transport\n"
+                            + "=============================== OUT TRANSACTIONS ===============================\n"
+                    , outputStream.toString());
+        }
+    }
+    @Nested
+    class SaveToFile {
+        @BeforeEach
+        void populateStateManager() {
+            try {
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                Ui ui = new Ui(outputStream);
+                Parser parser = new Parser();
+                parser.parse("goal /add car /amount 1000").execute(ui);
+                parser.parse("goal /add ps5 /amount 1000").execute(ui);
+                parser.parse("in part-time job /amount 1000 /goal car").execute(ui);
+                parser.parse("in allowance /amount 500 /goal car /recurrence monthly").execute(ui);
+                parser.parse("in sell stuff /amount 50 /goal ps5").execute(ui);
+                parser.parse("out buy dinner /amount 15 /category food /recurrence daily").execute(ui);
+                parser.parse("out popmart /amount 12 /category toy").execute(ui);
+                parser.parse("out grab /amount 20 /category transport").execute(ui);
+            } catch (DukeException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        @AfterEach
+        void clearStateManager() {
+            File file = new File("category-store.csv");
+            file.delete();
+            file = new File("goal-store.csv");
+            file.delete();
+            file = new File("expense-store.csv");
+            file.delete();
+            file = new File("income-store.csv");
+            file.delete();
+            StateManager.clearStateManager();
+        }
+
+        @Test
+        void saveDataWorking() throws DukeException, IOException {
+            storage.save();
+            File output = new File("category-store.csv");
+            File testFile = new File("./TestCSV/valid/category-store.csv");
+            assertEquals(true, FileUtils.contentEquals(output, testFile));
+            output = new File("goal-store.csv");
+            testFile = new File("./TestCSV/valid/goal-store.csv");
+            assertEquals(true, FileUtils.contentEquals(output, testFile));
+            output = new File("income-store.csv");
+            testFile = new File("./TestCSV/valid/income-store.csv");
+            assertEquals(true, FileUtils.contentEquals(output, testFile));
+            output = new File("expense-store.csv");
+            testFile = new File("./TestCSV/valid/expense-store.csv");
+            assertEquals(true, FileUtils.contentEquals(output, testFile));
         }
     }
 }
