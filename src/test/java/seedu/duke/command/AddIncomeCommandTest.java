8e961dc77f44f09636b2f4c26b28f618dc433e06
hooami <hoojunhong.17@ichat.sp.edu.sg>
2023-11-09 22:53:39 +0800

    Change test case for AddIncomeCommandTest

--- src/test/java/seedu/duke/command/AddIncomeCommandTest.java
+++ src/test/java/seedu/duke/command/AddIncomeCommandTest.java
@@ -77,8 +77,8 @@ class AddIncomeCommandTest {
                             "weekly\n",
                     () -> {
                         ArrayList<Income> incomes = StateManager.getStateManager().getAllIncomes();
-                        Income lastAddedIncome = incomes.get(incomes.size() - 1);
-                        assertEquals(lastAddedIncome.getTransaction().getRecurrence(), TransactionRecurrence.NONE);
+                        Income lastAddedIncome = incomes.get(1);
+                        assertEquals(lastAddedIncome.getTransaction().getRecurrence(), TransactionRecurrence.WEEKLY);
                     }
             )
         };
